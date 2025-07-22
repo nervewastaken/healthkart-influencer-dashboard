b945ef878a5950fcc8f3e2ea6d9416330ffd515a
nervewastaken <krishverma2004@gmail.com>
2025-07-22 18:16:24 +0530

    really hot fix

--- main.py
+++ main.py
@@ -2675,4 +2675,4 @@ def toggle_help_modal(help_clicks, close_clicks, current_style):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8050)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8050)))
